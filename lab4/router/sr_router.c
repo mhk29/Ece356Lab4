a54e900328c0c189ee556a28a0040d8ea27525c9
mhk29 <mhk29@duke.edu>
2021-04-10 10:00:07 -0400

    reverse

--- lab4/router/sr_router.c
+++ lab4/router/sr_router.c
@@ -325,10 +325,10 @@ void sr_handleippacket(struct sr_instance  *sr,
       print_hdr_ip((uint8_t *)send_ip_head);
 
       /* Step 2bii2e: Prepare Ethernet Header */
-      /* struct sr_arpentry *arp_entry = sr_arpcache_lookup(&(sr->cache), ip_head->ip_src); */
+      struct sr_arpentry *arp_entry = sr_arpcache_lookup(&(sr->cache), ip_head->ip_src);
 
-      memcpy(send_ethernet_head->ether_dhost, o_interface->addr, sizeof(o_interface->addr));
-      memcpy(send_ethernet_head->ether_shost, eth_head->ether_dhost, sizeof(eth_head->ether_dhost));
+      memcpy(send_ethernet_head->ether_dhost, arp_entry->mac, sizeof(arp_entry->mac));
+      memcpy(send_ethernet_head->ether_shost, t_iface->addr, sizeof(t_iface->addr));
       send_ethernet_head->ether_type = eth_head->ether_type;
 
       print_hdr_eth((uint8_t *)send_ethernet_head);
@@ -337,7 +337,7 @@ void sr_handleippacket(struct sr_instance  *sr,
       sr_send_packet(sr, send_icmp, outgoing_len, interface);
       
       /* Free and Return */
-      /* free(arp_entry); */
+      free(arp_entry);
       free(send_icmp);
       return;
 
