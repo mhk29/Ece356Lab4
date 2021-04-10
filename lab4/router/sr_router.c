e5983e57c054a23d7ccd931eefa9ecd60d9db479
mhk29 <mhk29@duke.edu>
2021-04-10 10:03:56 -0400

    back again

--- lab4/router/sr_router.c
+++ lab4/router/sr_router.c
@@ -400,8 +400,8 @@ void sr_handleippacket(struct sr_instance  *sr,
       /* Step 2cie: Prepare Ethernet Header */
       struct sr_arpentry *arp_entry = sr_arpcache_lookup(&(sr->cache), ip_head->ip_src); 
 
-      memcpy(send_ethernet_head->ether_dhost, o_interface->addr, sizeof(o_interface->addr));
-      memcpy(send_ethernet_head->ether_shost, eth_head->ether_dhost, sizeof(eth_head->ether_dhost));
+      memcpy(send_ethernet_head->ether_dhost, arp_entry->mac, sizeof(arp_entry->mac));
+      memcpy(send_ethernet_head->ether_shost, t_iface->addr, sizeof(t_iface->addr));
       send_ethernet_head->ether_type = eth_head->ether_type;
 
       print_hdr_eth((uint8_t *)send_ethernet_head);
