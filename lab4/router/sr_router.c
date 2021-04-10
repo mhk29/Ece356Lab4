45732bd67df62488cee3f2284f633b5053832b31
mhk29 <mhk29@duke.edu>
2021-04-10 10:09:35 -0400

    wrf

--- lab4/router/sr_router.c
+++ lab4/router/sr_router.c
@@ -459,9 +459,9 @@ void sr_handleippacket(struct sr_instance  *sr,
       print_hdr_ip((uint8_t *)send_ip_head);
 
       /* Step 2ciie: Prepare Ethernet Header */
-      /* struct sr_arpentry *arp_entry = sr_arpcache_lookup(&(sr->cache), ip_head->ip_src);  */
+      struct sr_arpentry *arp_entry = sr_arpcache_lookup(&(sr->cache), ip_head->ip_src);  
 
-      memcpy(send_ethernet_head->ether_dhost, o_interface->addr, sizeof(o_interface->addr));
+      memcpy(send_ethernet_head->ether_dhost, arp_entry->mac, sizeof(o_interface->addr));
       memcpy(send_ethernet_head->ether_shost, t_iface->addr, sizeof(t_iface->addr));
       send_ethernet_head->ether_type = eth_head->ether_type;
 
@@ -471,7 +471,7 @@ void sr_handleippacket(struct sr_instance  *sr,
       sr_send_packet(sr, send_icmp, outgoing_len, interface);
       
       /* Free and Return */
-      /* free(arp_entry); */
+      free(arp_entry); 
       free(send_icmp);
       return;
     }
